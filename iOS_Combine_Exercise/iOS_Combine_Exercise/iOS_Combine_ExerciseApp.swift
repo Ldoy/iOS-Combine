932f02ed4b0e291878e25d9089b9711b4b9e86cc
Ldoy <leedoy7989@gmail.com>
2021-11-05 10:29:37 +0900

    feat: Add Project

---
+++ iOS_Combine_Exercise/iOS_Combine_Exercise/iOS_Combine_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Combine_ExerciseApp.swift
+//  iOS_Combine_Exercise
+//
+//  Created by Do Yi Lee on 2021/11/05.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Combine_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
